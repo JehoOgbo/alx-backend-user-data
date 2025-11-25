6fb55c92e72006eb2cdbad70531b447fde550120
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-11-25 21:24:39 +0100

    Basic - base64 decode
    Function to decode a base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """ BasicAuth class"""
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ class to handle basic authentication
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ Get the Base64 part of the authorization header for
             a basic authentication
         """
@@ -19,5 +21,23 @@ class BasicAuth(Auth):
             if test is False:
                 return None
             return authorization_header[6:]
+        except Exception as e:
+            return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode the base 64 in the authorization header
+            to get the encoded information
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            result = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+            return result.decode('utf-8')
         except Exception as e:
             return None
