92c5844bfea611d0725fe6f7f5c6c3fdea1345da
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-11-25 18:34:59 +0100

    Auth class
    Template for different kinds of user authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Auth class """
+from flask import request
+from typing import List, TypeVar
+
+
+
+class Auth:
+    """ Template for authentication systems
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ returns None
+          - request is a flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None
+          - request will be a flask request object
+        """
+        return None
