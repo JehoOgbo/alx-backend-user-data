4420f2c4d4979003f8137166875c49737ff1fe3e
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-11-26 18:45:28 +0100

    Create and Retrieve
    Implement functions to create and retrieve sessions attached to a user
    login

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 """ SessionAuth class"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Define functions that implement session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a new session id for the user
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        iden = str(uuid4())
+        self.user_id_by_session_id[iden] = user_id
+        return iden
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Get the user_id attached to a session_id
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
