78c9ce5f2edeb53d29e499f2918bd12fc8cca0f8
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-11-26 09:26:33 +0100

    Get user credentials
    Function to get user credentials from a basic auth request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,16 @@ class BasicAuth(Auth):
             return result.decode('utf-8')
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Get the user email and password from the decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':')
+        return email, password
