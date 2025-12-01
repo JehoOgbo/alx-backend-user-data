b8b765eca074f95066828490d62eb5cd5a1a23ec
Urom Jehoshaphat Ogbonnia <jehoshaphaturom@gmail.com>
2025-12-01 09:33:33 +0100

    Add new route for user creation
    Add api endpoint to create users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app
 """
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request
 from auth import Auth
 
 
@@ -17,6 +17,24 @@ def hello_world():
         Return a simple json
     """
     return jsonify({'message': 'Bienvenue'})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def create_users():
+    """ Endpoint to create User objects
+
+      - Expects email and password
+      - Usage:
+            POST /users -d {"email": "haha@gpa.com", "password": "***"}
+    """
+    if request:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            new_user = AUTH.register_user(email, password)
+            return jsonify({"email": new_user.email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
+    return jsonify({"error": "missing fields"}), 400
 
 
 if __name__ == "__main__":
